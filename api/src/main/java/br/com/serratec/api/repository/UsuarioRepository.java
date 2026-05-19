663c69d56a30aff5579916e0eb8e793ccbe37ceb
nisgri@hotmail.com <nicolas@MacBook-Pro-de-Nicolas.local>
2026-05-19 11:07:06 -0300

    adicionado exemplos da aula 8

---
+++ api/src/main/java/br/com/serratec/api/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package br.com.serratec.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.serratec.api.model.Usuario;
+
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+    Usuario findByEmail(String email);
+}
